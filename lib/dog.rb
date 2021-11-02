1915fb192748fdcd3841143660f61683df41d197
Andrea Freeman <afreeman5566@gmail.com>
2021-11-02 18:03:15 -0500

    commit tree 11081a3bcd8efa176835736559f30b6e6aff5216

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,18 @@
-class Dog
 
+
+class Dog
+    def name= (dog_name)
+        @this_dogs_name = dog_name  #getter variable
+    end
+
+    def name
+        @this_dogs_name      #setter variable
+    end
 end
+
+lassie = Dog.new
+lassie.name = "Lassie"
+
+puts lassie.name
+
+
